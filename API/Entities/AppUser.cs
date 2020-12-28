24c8049302db45aa90753c6623c5773694c7f5db
Rich Staples <richstaples@users.noreply.github.com>
2020-12-28 18:43:49 -0500

    Lesson 9

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Entities
+{
+    /// <summary>
+    /// Class for a user of the application
+    /// </summary>
+    public class AppUser
+    {
+        public int Id { get; set; }
+        
+        public string Username { get; set; }
+        
+    }
+}
